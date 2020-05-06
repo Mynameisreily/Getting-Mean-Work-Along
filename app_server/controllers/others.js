a1bcce0063021a0d85416d026366c1bc5e155c63
Reily Petty <rpetty@appirio.com>
2020-05-06 19:35:29 -0400

    End of Chapter 3, Finally got Google Maps API working

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,4 +1,8 @@
 /* GET home page */
 module.exports.about = function (req, res) {
-  res.render("generic-text", { title: "About" });
+  res.render("generic-text", {
+    title: "About Loc8r",
+    content:
+      "Loc8r was created to help people find places to sit down and get a bit of work done\n\nLorem ipsus dolor sit amet, consectetur adipiscing elit. Nunc sed lorem ac nisi dignissim accumsian",
+  });
 };
