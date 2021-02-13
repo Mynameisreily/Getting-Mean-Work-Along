fad350d2517ff681a1d314d509a866c8cb685d7b
Reily Petty <reilypetty96@gmail.com>
2021-02-13 15:33:09 -0500

    Database Call and Rendering add

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -7,7 +7,12 @@ if(process.env.NODE_ENV === 'production') {
   apiOptions.server = "https://stark-falls-30670.herokuapp.com";
 }
 
-module.exports.homelist = function (req, res) {
+//URL - apiOptions
+//method
+//body
+//querystring
+
+var renderHomepage = function (req, res, responseBody) {
   res.render("locations-list", {
     title: "Loc8r - find a place to work with wifi",
     pageHeader: {
@@ -16,30 +21,29 @@ module.exports.homelist = function (req, res) {
     },
     sidebar:
       "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for.",
-    locations: [
-      {
-        name: "Starcups",
-        address: "125 High Street, Reading, RG6 1PS",
-        rating: 3,
-        facilities: ["Hot drinks", "Food", "Premium wifi"],
-        distance: "100m",
-      },
-      {
-        name: "Donkin Duns",
-        address: "420 Toasted Ave, Leeds, RG8 2PS",
-        rating: 5,
-        facilities: ["America", "Run No", "Dun Dun"],
-        distance: "150",
-      },
-      {
-        name: "Wet Paper Straw",
-        address: "234 Black Road, London, RG5 3PS",
-        rating: 2,
-        facilities: ["Cold drinks", "Paper Straws", "Slow wifi"],
-        distance: "250m",
-      },
-    ],
+    locations: responseBody
   });
+};
+
+module.exports.homelist = function (req, res) {
+  var requestOptions, path;
+  path = '/api/locations';
+  requestOptions = {
+    url : apiOptions.server + path,
+    method : "GET",
+    json : {},
+    qs : {
+      lng : -0.7992599,
+      lat : 51.378091,
+      maxDistance : 20
+    }
+  };
+  request(
+    requestOptions,
+    function(err, response, body) {
+      renderHomepage(req, res, body);
+    }
+  );
 };
 
 /* GET 'Location info' Page */
