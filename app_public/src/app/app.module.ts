fd54c916cf62a62e186655e7adefc8362efeee79
Reily Petty <reilypetty96@gmail.com>
2021-02-27 11:55:02 -0500

    Section 9.1 Heroku Deploy

--- app_public/src/app/app.module.ts
+++ app_public/src/app/app.module.ts
@@ -1,20 +1,37 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { HomeListComponent } from './home-list/home-list.component';
 import { DistancePipe } from './distance.pipe';
+import { FrameworkComponent } from './framework/framework.component';
+import { AboutComponent } from './about/about.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: HomeListComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  }
+]
 
 @NgModule({
   declarations: [
     HomeListComponent,
-    DistancePipe
+    DistancePipe,
+    FrameworkComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
-  bootstrap: [HomeListComponent]
+  bootstrap: [FrameworkComponent]
 })
 export class AppModule { }
